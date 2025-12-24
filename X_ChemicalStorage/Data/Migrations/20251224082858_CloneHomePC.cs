ddfc275f20a4295291291eb513bc77c50e5faebe
Pc <Pc@DESKTOP-J4R0M3U>
2025-12-24 10:32:12 +0200

    Clone Project To Pc Home!

---
+++ X_ChemicalStorage/Data/Migrations/20251224082858_CloneHomePC.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace X_ChemicalStorage.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CloneHomePC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
