b962b661bd3dc5e9149344a27cc0750a138b30de
Ahmed Desoki <adesokiii88@gmail.com>
2026-01-25 15:22:35 +0200

    dashboard refactor and refactor some errors

---
+++ X_ChemicalStorage/Data/Migrations/20260125111118_RequiredItemEntity.cs
@@ -0,0 +1,231 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace X_ChemicalStorage.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RequiredItemEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Locations_LocationId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Units_UnitId",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UnitId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageCondition",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "SDS",
+                table: "Items",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BarcodeImage",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Locations_LocationId",
+                table: "Items",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Units_UnitId",
+                table: "Items",
+                column: "UnitId",
+                principalTable: "Units",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Locations_LocationId",
+                table: "Items");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Units_UnitId",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UnitId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StorageCondition",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "SDS",
+                table: "Items",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Code",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Items",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BarcodeImage",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Categories_CategoryId",
+                table: "Items",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Locations_LocationId",
+                table: "Items",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Units_UnitId",
+                table: "Items",
+                column: "UnitId",
+                principalTable: "Units",
+                principalColumn: "Id");
+        }
+    }
+}
--- X_ChemicalStorage/Models/Item.cs
+++ X_ChemicalStorage/Models/Item.cs
@@ -8,24 +8,34 @@ namespace X_ChemicalStorage.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
         public string? Code { get; set; } // item Code // الكود الذي سيولد منه الباركود
+        [Required]
         public string? Name { get; set; } // item Name
+        [Required]
         public bool? SDS { get; set; } //  Safty Data Sheet 
+        [Required]
         public int Limit { get; set; } = 1; // Minimum Limit
+        
         public double? TotalQuantity { get; set; } = 0;
         public double? AvilableQuantity { get; set; } = 0;
+        [Required]
         public string? StorageCondition { get; set; } // Storage Condition Details 
+        [Required]
         public string? BarcodeImage { get; set; } // مسار صورة الباركود
 
         // Foreign Keys
+        [Required]
         public int? CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category? Category { get; set; }
         // Foreign Keys
+        [Required]
         public int? UnitId { get; set; }
         [ForeignKey("UnitId")]
         public Unit? Unit { get; set; }
         // Foreign Keys
+        [Required]
         public int? LocationId { get; set; } // Location Details
         [ForeignKey("LocationId")]
         public Location? Location { get; set; }
